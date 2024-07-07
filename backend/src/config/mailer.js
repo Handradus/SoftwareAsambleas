fe7f0d4abbe597bed28d8d18442cf9dd48abc19e
Handrades <humberto.andrades2201@alumnos.ubiobio.cl>
2024-07-06 21:55:12 -0400

    inicializacion de nodemail

---
+++ backend/src/config/mailer.js
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+   
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.log('Error en la configuración del transportador:', error);
+    } else {
+        console.log('Transportador de correo configurado correctamente');
+    }
+});
+
+export default transporter;
