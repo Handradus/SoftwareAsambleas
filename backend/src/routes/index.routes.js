67f61f9b9fc65a61423c94cef4586ab0943c252b
Handrades <humberto.andrades2201@alumnos.ubiobio.cl>
2024-07-08 16:09:43 -0400

    importe controllers de directivas

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -8,6 +8,11 @@ import userRoutes from "./user.routes.js";
 /** Enrutador de autenticación */
 import authRoutes from "./auth.routes.js";
 
+/// enrutador para vocalias
+import { createDirectiva, getDirectiva, updateDirectiva, deleteDirectiva } from '../controllers/directiva.controller.js';
+import { createVocalia, getVocalias, updateVocalia, deleteVocalia } from '../controllers/vocalia.controller.js';
+
+
 // Se realiza una instancia de express
 const router = Router();
 
@@ -16,9 +21,9 @@ router.use("/user",  userRoutes);
 // Define las rutas para la autenticación /api/auth
 router.use("/auth", authRoutes);
 
-router.use("/directivas",directivaRoutes);
+router.use("/directivas",getDirectiva);
 
-router.use("/vocalias",vocaliaRoutes);
+router.use("/vocalias",getVocalias);
 
 
 export default router;
